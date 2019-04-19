3d05b39d1325b9218d64ae0b3c1e86cd511e0b83
KarlRong <sjturjk@icloud.com>
2019-04-19 13:53:07 +0800

    occ, visualization

---
+++ data/NarrowPassDataset.py
@@ -0,0 +1,23 @@
+from torch.utils.data import Dataset
+import numpy as np
+import pandas as pd
+
+class NarrowPassDataset(Dataset):
+    def __init__(self, csv_file, sample_dim, condition_dim, occ_dim, train=True, ratio_test=0.8):
+        self.sample_dim = sample_dim
+        self.con_dim = condition_dim
+        self.occ_dim = occ_dim
+        self.dataset = np.array(pd.read_csv(csv_file, header=None), dtype='float32')
+        numTrain = int(ratio_test * len(self.dataset))
+        if (train):
+            self.dataset = self.dataset[:numTrain, :]
+        else:
+            self.dataset = self.dataset[numTrain:, :]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx, :self.sample_dim], \
+                self.dataset[idx, self.sample_dim:(self.sample_dim + self.con_dim)], \
+                self.dataset[idx, (self.sample_dim + self.con_dim):]
