519e1e0c9de5ed6a60d7bb71cd4963fafffd6747
rong <sjturjk@icloud.com>
2019-06-26 12:41:07 +0800

    fix errors in data utils

--- data/AttentionDataset.py
+++ data/AttentionDataset.py
@@ -4,48 +4,31 @@ import pandas as pd
 
 
 class AttentionDataset(Dataset):
-    def __init__(self, csv_file_data, csv_file_occ, sample_dim, condition_dim, gridSize, train=True, ratio_test=0.8):
+    def __init__(self, np_file_data, sample_dim, condition_dim, gridSize, train=True, ratio_test=0.8):
         self.sample_dim = sample_dim
         self.con_dim = condition_dim
         self.gridSize = gridSize
         self.occ_dim = gridSize**2
-        self.dataset = np.array(pd.read_csv(csv_file_data, header=None), dtype='float32')
-        self.occ = np.array(pd.read_csv(csv_file_occ, header=None), dtype='bool')
-        numTrain = int(ratio_test * len(self.dataset))
+        data = np.load(np_file_data)
+        self.sampleData = data['sampleData']
+        self.gapData = data['gapData']
+        self.occData = data['occ'].astype('float32')
+        numTrain = int(ratio_test * len(self.sampleData))
         if (train):
-            self.dataset = self.dataset[:numTrain, :]
-            self.occ = self.occ[:numTrain, :]
+            self.sampleData = self.sampleData[:numTrain, :]
+            self.gapData = self.gapData[:numTrain, :]
+            self.occData = self.occData[:numTrain, :]
         else:
-            self.dataset = self.dataset[numTrain:, :]
-            self.occ = self.occ[:numTrain, :]
+            self.sampleData = self.sampleData[numTrain:, :]
+            self.gapData = self.gapData[numTrain:, :]
+            self.occData = self.occData[numTrain:, :]
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.sampleData)
 
     def __getitem__(self, idx):
-        return self.dataset[idx, :self.sample_dim], \
-                self.dataset[idx, self.sample_dim:(self.sample_dim + self.con_dim)], \
-                self.occ[idx].reshape((1, self.gridSize, self.gridSize))
+        return self.sampleData[idx, :], \
+                self.gapData[idx, :], \
+                self.gapData[idx, -self.sample_dim*2:], \
+                self.occData[idx].reshape((1, self.gridSize, self.gridSize))
 
-
-class AttentionDataset(Dataset):
-    def __init__(self, csv_file_, sample_dim, condition_dim, gridSize, train=True, ratio_test=0.8):
-        self.sample_dim = sample_dim
-        self.con_dim = condition_dim
-        self.gridSize = gridSize
-        self.occ_dim = gridSize**2
-        self.dataset = np.array(pd.read_csv(csv_file, header=None), dtype='float32')
-        numTrain = int(ratio_test * len(self.dataset))
-        if (train):
-            self.dataset = self.dataset[:numTrain, :]
-        else:
-            self.dataset = self.dataset[numTrain:, :]
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        return self.dataset[idx, :self.sample_dim], \
-               self.dataset[idx, self.sample_dim:(self.sample_dim + self.con_dim)], \
-               self.dataset[idx, (self.con_dim - self.sample_dim):(self.sample_dim + self.con_dim)], \
-               self.dataset[idx, -self.occ_dim:].reshape((1, self.gridSize, self.gridSize))
--- utils/NarrowPassage.py
+++ utils/NarrowPassage.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.gridspec as gridspec
 import numpy as np
-
+import torch
 
 def isSampleFree(sample, obs, dimW):
     for o in range(0, obs.shape[0] // (2 * dimW)):
@@ -125,4 +125,49 @@ def plotSpeed(s, c):
     plt.scatter(s[:, viz1], s[:, viz2], color="green", s=70, alpha=0.1)
     plt.scatter(c[viz1 + 9], c[viz2 + 9], color="red", s=250, edgecolors='black')  # init
     plt.scatter(c[viz1 + 9 + dim], c[viz2 + 9 + dim], color="blue", s=500, edgecolors='black')  # goal
+    plt.show()
+
+
+def plotSampleAttention(s, condition, occ, attention):
+    fig1 = plt.figure(figsize=(30, 6), dpi=80)
+    ax1 = fig1.add_subplot(131, aspect='equal')
+
+    plt.scatter(s[:, 0], s[:, 1], color="green", s=70, alpha=0.1)
+
+    obs, dimW = gap2obs(condition)
+    for i in range(0, obs.shape[0] // (2 * dimW)):  # plot obstacle patches
+        ax1.add_patch(
+            patches.Rectangle(
+                (obs[i * 2 * dimW], obs[i * 2 * dimW + 1]),  # (x,y)
+                obs[i * 2 * dimW + dimW] - obs[i * 2 * dimW],  # width
+                obs[i * 2 * dimW + dimW + 1] - obs[i * 2 * dimW + 1],  # height
+                alpha=0.6
+            ))
+    gridSize = 11
+    occGrid = gap2occ(condition, gridSize)
+    occGridSamples = getOccGrid(gridSize)
+    for i in range(0, gridSize * gridSize):  # plot occupancy grid
+        if occGrid[i] == 0:
+            plt.scatter(occGridSamples[i, 0], occGridSamples[i, 1], color="red", s=70, alpha=0.8)
+        else:
+            plt.scatter(occGridSamples[i, 0], occGridSamples[i, 1], color="green", s=70, alpha=0.8)
+
+    init = condition[9:15]
+    goal = condition[15:21]
+    plt.scatter(init[0], init[1], color="red", s=250, edgecolors='black')  # init
+    plt.scatter(goal[0], goal[1], color="blue", s=250, edgecolors='black')  # goal
+    ax1.set_xlim([-0.1, 1.1])
+    ax1.set_ylim([-0.1, 1.1])
+
+    # plot attention
+    ax2 = fig1.add_subplot(132, aspect='equal')
+    img = torch.from_numpy(attention).permute(1, 0)
+    ax2.imshow(img)
+    ax2.invert_yaxis()
+
+    # plot occ
+    ax3 = fig1.add_subplot(133, aspect='equal')
+    occ = torch.from_numpy(occ).permute(1, 0)
+    ax3.imshow(occ)
+    ax3.invert_yaxis()
     plt.show()
